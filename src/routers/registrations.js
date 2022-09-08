698bba24adecfd9640a519bc7390b2e70ba0fbe1
Lorel <lorel.oboue@gmail.com>
2022-09-08 12:50:49 +0100

    updated

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -1,19 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
-    // Save the user using the prisma user model, setting their password to the hashed version
-    
-    // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+router.post("/", async (req, res) => {
+  // Get the username and password from request body
+  const data = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+
+  // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
+  // created a hashed version of the password, save it in a variable
+
+  const hashed = await bcrypt.hash(data.password, 10);
+  console.log(hashed);
+
+  // Save the user using the prisma user model, setting their password to the hashed version
+  //use prisma seeding method to create a user and set their password to the hashed version - how it get stored in stored in database
+  const createUser = await prisma.user.create({
+    data: {
+      username: data.username,
+      password: hashed,
+    },
+  });
+
+  // Respond back to the client with the created users username and id
+  res.status(201).json({ username: createUser.username, id: createUser.id });
+
+  // do not send passwords back to client
 });
 
 module.exports = router;
--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -1,22 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-    // Get the username and password from the request body
+router.post("/", async (req, res) => {
+  // Get the username and password from the request body
+  const { username, password } = req.body;
 
-    // Check that a user with that username exists in the database
-    // Use bcrypt to check that the provided password matches the hashed password on the user
-    // If either of these checks fail, respond with a 401 "Invalid username or password" error
+  // Check that a user with that username exists in the database
+  //prisma function would be findUnique
+  // const foundUser the uses the correct prisma, findUnique to find the user by their username
+  // make an if statement for if user is not found, error message
 
-    // If the user exists and the passwords match, create a JWT containing the username in the payload
-    // Use the JWT_SECRET environment variable for the secret key
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
 
-    // Send a JSON object with a "token" key back to the client, the value is the JWT created
+  if (!foundUser) {
+    return res.status(401).json({ msg: "Invalid username or password" });
+  }
+
+  // Use bcrypt to check that the provided password matches the hashed password on the user
+  const comparePassword = await bcrypt.compare(password, foundUser.password);
+
+  // If either of these checks fail, respond with a 401 "Invalid username or password" error
+  if (!comparePassword) {
+    return res.status(401).json({ msg: "Invalid username or password" });
+  }
+
+  // If the user exists and the passwords match, create a JWT containing the username in the payload
+  // Use the JWT_SECRET environment variable for the secret key
+
+  const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+  // Send a JSON object with a "token" key back to the client, the value is the JWT created
+  res.json({ token });
 });
 
 module.exports = router;
